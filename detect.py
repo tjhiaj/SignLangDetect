6d56df0a5232b14db77170fc675c8de744e7d145
tjhiaj <j.tjhia@gmail.com>
2024-02-02 18:26:08 -0500

    set up mediapipe model for detection

--- detect.py
+++ detect.py
@@ -5,19 +5,27 @@ from matplotlib import pyplot as plt
 import time
 import mediapipe as mp
 
-# grab cam, device 0
-cap = cv2.VideoCapture(0)
+mp_holistic = mp.solutions.holistic     #downloads model, makes detections
+mp_drawing = mp.solutions.drawing_utils # helps draw keypoints
 
-# loop through all frames
-while cap.isOpened():
-    # read current feed, 2 return values 
-    ret, frame = cap.read()
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # bgr to rgb
+    image.flags.writeable = False                  # save memory
+    results = model.process(image)                 # predict
+    image.flags.writeable = True                   # image now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # rgb to bgr
+    return image, results
 
-    # show to screen image (not ret)
-    cv2.imshow('OpenCV Feed', frame)
+cap = cv2.VideoCapture(0) # grab cam, device 0
+# set model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # loop through all frames
+    while cap.isOpened():                      
+        ret, frame = cap.read()                               # read current feed, 2 return values 
+        image, results = mediapipe_detection(frame, holistic)
+        cv2.imshow('OpenCV Feed', frame)                      # show to screen image (not ret)
+        if cv2.waitKey(10) & 0xFF == ord('q'):                # wait for exit key press
+            break
+    cap.release()
+    cv2.destroyAllWindows()    
 
-    # wait for key press
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()    
