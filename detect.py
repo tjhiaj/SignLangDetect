1c7b414928fea9c16d4655cba14289cc8f73486e
tjhiaj <j.tjhia@gmail.com>
2024-02-02 16:23:25 -0500

    set up opencv feed w/ webcam

--- detect.py
+++ detect.py
@@ -4,3 +4,20 @@ import os
 from matplotlib import pyplot as plt
 import time
 import mediapipe as mp
+
+# grab cam, device 0
+cap = cv2.VideoCapture(0)
+
+# loop through all frames
+while cap.isOpened():
+    # read current feed, 2 return values 
+    ret, frame = cap.read()
+
+    # show to screen image (not ret)
+    cv2.imshow('OpenCV Feed', frame)
+
+    # wait for key press
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()    
