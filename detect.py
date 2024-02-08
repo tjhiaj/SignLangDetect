d67b5845788686d251aab4863ddee3dbe7995aaa
tjhiaj <j.tjhia@gmail.com>
2024-02-07 21:07:24 -0500

    styled landmark rendering

--- detect.py
+++ detect.py
@@ -18,10 +18,18 @@ def mediapipe_detection(image, model):
 
 # render landmarks
 def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, 
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1), 
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1))
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4), 
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4), 
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2))
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4), 
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
 
 cap = cv2.VideoCapture(0) # grab cam, device 0
 # set model
