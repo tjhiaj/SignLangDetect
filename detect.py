f51e69780f6a850d84a2b4c237aa899a87638d74
tjhiaj <j.tjhia@gmail.com>
2024-02-10 20:30:27 -0500

    wrapped keypoint array extraction into function

--- detect.py
+++ detect.py
@@ -48,3 +48,12 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 # draw_landmarks(frame, results)
 # plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 # plt.show()
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(112)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(63) #so no error thrown when hand out of frame
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(63)
+    return np.concatenate([pose, face, lh, rh])
+
+print(extract_keypoints(results)).shape
