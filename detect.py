f26f105e52129c357c6b29b173c69cd3b5deff95
tjhiaj <j.tjhia@gmail.com>
2024-03-04 23:35:59 -0500

    render results onscreen

--- detect.py
+++ detect.py
@@ -144,7 +144,7 @@ print(accuracy_score(ytrue, yhat))
 #new detectin vars for real time predictions
 sequence = []   # 30 frames
 sentence = []   # concatenate history of detections
-threshold = 0.4 # only render results if above this
+threshold = 0.7 # only render results if above this
 
 cap = cv2.VideoCapture(0) # grab cam, device 0
 # set model
@@ -161,6 +161,22 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         if len(sequence) == 30:
             res = model.predict(np.expand_dims(sequence, axis=0))[0]
             print(actions[np.argmax(res)])
+
+            # visualization
+            if res[np.argmax(res)] > threshold:
+                if len(sentence) > 0:
+                    if actions[np.argmax(res)] != sentence[-1]: #check if current word is different from last
+                        sentence.append(actions[np.argmax(res)])
+                else:
+                    print(actions[np.argmax(res)])
+                    sentence.append(actions[np.argmax(res)])
+            
+            if len(sentence) > 5:
+                sentence = sentence[-5:]
+
+        #rendering
+        cv2.rectangle(image, (0,0), (640,40), (245,117,16), -1) # -1 means fill
+        cv2.putText(image, ' '.join(sentence), (3,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
 
         cv2.imshow('OpenCV Feed', image)                      # show to screen image (not ret)
         if cv2.waitKey(10) & 0xFF == ord('q'):                # wait for exit key press
